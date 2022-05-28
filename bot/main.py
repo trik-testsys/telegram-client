9735324bbd4fc4a7d98173cf45d2b9947672d519
KarasssDev <viktorkarasev27@gmail.com>
2022-05-28 13:25:59 +0300

    added utils

--- bot/main.py
+++ bot/main.py
@@ -1,15 +1,9 @@
 import logging
 
-from bot.conf import PATH_TO_KEY
+
 from bot.teletrik.Client import Client
+from bot.utils import _read_key
 import controller
-
-
-def _read_key() -> str:
-    key_file = open(PATH_TO_KEY, "r")
-    api_key = key_file.read()
-    key_file.close()
-    return api_key
 
 
 if __name__ == "__main__":
