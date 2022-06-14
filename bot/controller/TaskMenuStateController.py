65df17914307b41a0d08b397cfc12a28cd122cce
KarasssDev <viktorkarasev27@gmail.com>
2022-06-14 13:51:53 +0300

    TaskMenu controller now handle files

--- bot/controller/TaskMenuStateController.py
+++ bot/controller/TaskMenuStateController.py
@@ -1,9 +1,10 @@
 from aiogram.types import KeyboardButton, Message, ParseMode, ReplyKeyboardMarkup
 import aiogram.utils.markdown as md
-from bot.controller.States import StudentMenu, SubmitTask, TaskMenu
+from bot.controller.States import StudentMenu, TaskMenu
 from bot.repository.StateInfoRepository import StateInfoRepository
 from bot.repository.SubmitRepository import SubmitRepository
 from bot.repository.TaskRepository import TaskRepository
+from bot.service.GradingService import GradingService
 from bot.service.LektoriumService import LektoriumService
 from bot.teletrik.Controller import Controller
 from bot.teletrik.DI import controller
@@ -19,21 +20,29 @@ class TaskMenuStateController(Controller):
         state_info_repository: StateInfoRepository,
         lektorium_service: LektoriumService,
         submit_view: SubmitView,
+        grading_service: GradingService,
     ):
         self.task_repository: TaskRepository = task_repository
         self.submit_repository: SubmitRepository = submit_repository
         self.state_info_repository: StateInfoRepository = state_info_repository
         self.lektorium_service: LektoriumService = lektorium_service
         self.submit_view: SubmitView = submit_view
+        self.grading_service: GradingService = grading_service
 
     SUBMIT_RESULTS = "Результаты запусков"
     DATA_FOR_LEKTORIUM = "Лучший результат"
-    SUBMIT = "Отправить ▸"
     BACK = "◂ Назад"
-
-    CHOOSE_ACTION = "Выберите действие"
+    SENT = "Бот принял вашу работу и начал её анализировать. Таблицу с результатами обработки можно посмотреть в меню 'Результаты запусков'"
+    NOT_SENT = (
+        "Решение не отправлено так как сервер проверки недоступен. Попробуйте позже"
+    )
+    ERROR_NOT_FILE = "Пожалуйста, отправьте файл"
 
     async def handle(self, message: Message):
+
+        is_submit: bool = await self.handle_submit(message)
+        if is_submit:
+            return TaskMenu
 
         match message.text:
 
@@ -42,11 +51,12 @@ class TaskMenuStateController(Controller):
                 results = await self.submit_view.get_student_submits_view(
                     state_info.user_id, state_info.chosen_task
                 )
-                await message.answer(md.code(results), parse_mode=ParseMode.MARKDOWN_V2)
+                await message.answer(
+                    md.code(results),
+                    parse_mode=ParseMode.MARKDOWN_V2,
+                    reply_markup=await self._create_keyboard(message)
+                )
                 return TaskMenu
-
-            case self.SUBMIT:
-                return SubmitTask
 
             case self.BACK:
                 return StudentMenu
@@ -80,6 +90,24 @@ class TaskMenuStateController(Controller):
                 )
                 return TaskMenu
 
+    async def handle_submit(self, message: Message) -> bool:
+
+        if message.document is None:
+            return False
+
+        document_id = message.document.file_id
+        file = await message.bot.download_file_by_id(document_id)
+        state_info = self.state_info_repository.get(message.from_user.id)
+        submit_id = await self.grading_service.send_task(
+            state_info.chosen_task, state_info.user_id, file
+        )
+
+        if submit_id != self.grading_service.ERROR:
+            await message.answer(f"{self.SENT}, ID решения: {submit_id}")
+        else:
+            await message.answer(f"{self.NOT_SENT}")
+        return True
+
     async def prepare(self, message: Message):
         await message.answer(
             await self._get_description(message),
@@ -88,7 +116,6 @@ class TaskMenuStateController(Controller):
 
     async def _create_keyboard(self, message: Message) -> ReplyKeyboardMarkup:
         task_menu_keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-        task_menu_keyboard.add(KeyboardButton(self.SUBMIT))
         task_menu_keyboard.add(KeyboardButton(self.SUBMIT_RESULTS))
 
         tg_id = message.from_user.id
